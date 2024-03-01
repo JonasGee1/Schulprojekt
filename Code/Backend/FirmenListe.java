0b0d2702983d92429e8a0371907131f2e1f25ce8
zcapok <stan.kopacz@googlemail.com>
2024-03-01 12:09:38 +0100

    Beginn der FirmenListe

---
+++ Code/Backend/FirmenListe.java
@@ -0,0 +1,19 @@
+package Code.Backend;
+
+import java.util.ArrayList;
+
+public class FirmenListe {
+    //private ArrayList<ZeitslotsVerteilung> verteiler = new ArrayList<>();
+    
+
+public static void main(String[] args) {
+    ArrayList<ZeitslotsVerteilung> verteiler = new ArrayList<>();
+    
+    System.out.println(verteiler.get(1));
+
+    
+}
+
+    
+
+}
---
+++ Code/Backend/Main.java
@@ -0,0 +1,7 @@
+package Code.Backend;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
